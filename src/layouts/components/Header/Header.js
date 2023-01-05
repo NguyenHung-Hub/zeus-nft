8a8d3c6fbf06a2b6f9606570e8454406008b85d9
Hung <daminhnguyenhung@gmail.com>
2023-01-05 14:39:35 +0700

    done header

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <div className={cx('wrapper')}>Header</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('nav-left')}>
+                <Link to={'/'}>
+                    <div className={cx('nav-item')}>Home</div>
+                </Link>
+                <Link to={'/'}>
+                    <div className={cx('nav-item')}>University</div>
+                </Link>
+                <Link to={'/'}>
+                    <div className={cx('nav-item')}>Courses</div>
+                </Link>
+            </div>
+
+            <div className={cx('nav-center')}>
+                <Link to={'/'}>
+                    <img src={images.logo} alt="Zeus" />
+                </Link>
+            </div>
+
+            <div className={cx('nav-right')}>
+                <Link to={'/'}>
+                    <div className={cx('nav-item')}>Use cases</div>
+                </Link>
+                <Link to={'/'}>
+                    <div className={cx('nav-item')}>Plans</div>
+                </Link>
+                <Link to={'/'}>
+                    <div className={cx('nav-item')}>Resource</div>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
